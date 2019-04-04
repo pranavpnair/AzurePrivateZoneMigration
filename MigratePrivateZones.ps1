036fa809619fca7742014a3f6a55dcdb6a294d56
Pranav Nair <pranavpradeepnair@gmail.com>
2019-04-04 11:12:32 -0700

    Update MigratePrivateZones.ps1

--- MigratePrivateZones.ps1
+++ MigratePrivateZones.ps1
@@ -1,3 +1,8 @@
+##############################################################################
+Copyright (c) Microsoft Corporation.  All rights reserved.
+##############################################################################
+
+
 param(
     [Parameter(Mandatory=$true)] [ValidateNotNullOrEmpty()] [string] $subscriptionId
 )
